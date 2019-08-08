a50eff8b71a626bc4df6c87315f011d52bcdc2e0
Vladimir Zaycev <vzaycev@nic.ru>
2019-08-08 16:14:44 +0300

    Function '*struct addnode(*struct, int foo)' -- adding a new element to the bintree

--- bintree.c
+++ bintree.c
@@ -7,6 +7,13 @@ struct btree_t {
 	struct btree_t * right;
 };
 
+struct btree_t * addnode (struct btree_t * tree, int value){
+	/* TODO: Add a new node to binary tree */
+	return someptr;
+}
+
 int main() {
+	struct btree_t * tree = NULL;
+	tree = addnode(tree, 10);
 	return 0;
 }
