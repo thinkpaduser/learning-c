7ffa52209415ad195e90e6a3776d1e85987597c2
= <vzaycev@nic.ru>
2019-09-19 19:31:20 +0300

    binary tree: adding new node

--- bintree.c
+++ bintree.c
@@ -1,24 +1,44 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-struct btree_t {
+struct bintree_t {
 	int value;
-	struct btree_t * left;
-	struct btree_t * right;
+	struct bintree_t * left;
+	struct bintree_t * right;
 };
 
-struct btree_t * addnode (struct btree_t * tree, int value){
-	/* TODO: Add a new node to binary tree */
+struct bintree_t * add_node (struct bintree_t * tree, int value){
 	if (tree == NULL) {
-		tree = malloc(sizeof(struct btree_t));
+		tree = malloc(sizeof(struct bintree_t));
 		tree->value = value;
+		return tree;
 	}
+	struct bintree_t * node = malloc(sizeof(struct bintree_t));
+	if (tree->value <= value) {
+		if (tree->right != NULL) {
+			add_node(tree->right, value);
+		} else {
+			tree->right = node;
+		}
+	} else if (tree->value > value) {
+		if (tree->left != NULL) {
+			add_node(tree->left, value);
+		} else {
+			tree->left = node;
+		}
+	}
+	node->value = value;
 	return tree;
 }
 
-int main() {
-	struct btree_t * tree = NULL;
-	tree = addnode(tree, 10);
-	printf("%d\n", tree->value);
+int main(){
+	struct bintree_t * tree = NULL;
+	tree = add_node(tree, 228);
+	tree = add_node(tree, 1337);
+	tree = add_node(tree, 1488);
+	tree = add_node(tree, 123);
+	tree = add_node(tree, 122);
+	tree = add_node(tree, 14);
+	printf("%d\t%d\t%d\t%d\t%d\n", tree->value, tree->right->value, tree->right->right->value, tree->left->value, tree->left->left->value);
 	return 0;
 }
